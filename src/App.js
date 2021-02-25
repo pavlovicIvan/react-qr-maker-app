c38b980b8c503dd366a3b2a97dc828a4d447b829
pavlovicIvan <ivanpavlo4@gmail.com>
2021-02-25 20:34:56 +0100

    Style updated and download function implemented

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import React from "react";
 import "./App.css";
 
 // Custom components
+import Header from "./layout/Header";
 import QRGenerator from "./components/QRGenerator";
 
-const App = () => <QRGenerator />;
+const App = () => (
+  <div className="containerCenter">
+    <Header />
+    <QRGenerator />
+  </div>
+);
 
 export default App;
--- src/components/QRGenerator.js
+++ src/components/QRGenerator.js
@@ -1,19 +1,33 @@
+// React
 import React, { useState } from "react";
+
+// Generator
 import QRCode from "qrcode.react";
 
+// Custom functions
+import downloadQR from "../helpers/functions";
+
 const QRGenerator = () => {
-  const [qrValue, setQrValue] = useState("QR Code");
+  const [qrValue, setQrValue] = useState("");
 
   return (
-    <div className="containerCenter">
-      <div className="containerMain">
-        <input
-          type="text"
-          onChange={(e) => setQrValue(e.target.value)}
-          value={qrValue}
-        />
-        <QRCode value={qrValue} size={300} includeMargin />
-      </div>
+    <div className="containerMain">
+      <textarea
+        autoFocus
+        rows="3"
+        onChange={(e) => setQrValue(e.target.value)}
+        value={qrValue}
+        placeholder="Type your text or url here"
+      />
+      <QRCode id="qr" value={qrValue} size={300} includeMargin />
+      <button
+        disabled={!qrValue}
+        type="button"
+        className="downloadButton"
+        onClick={() => downloadQR(qrValue)}
+      >
+        DOWNLOAD
+      </button>
     </div>
   );
 };
